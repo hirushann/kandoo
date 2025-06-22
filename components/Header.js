e16049b43455d861fbf1f47c1014cefe6fe5f9d1
hirushann <hirushan.perera2000@gmail.com>
2025-06-23 01:29:43 +0530

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <header
+      className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
+        scrolled ? 'bg-white shadow-md py-6' : 'bg-white py-8'
+      }`}
+    >
+      <div className="px-8 flex justify-between items-center transition-all duration-300">
+        <div className={`text-2xl font-bold ${scrolled ? 'text-black' : 'text-white'}`}>
+            <Link href="/">
+                <Image
+                src="/loora-logo.svg"
+                alt="Loora Logo"
+                width={112}
+                height={32}
+                priority
+                />
+            </Link>
+        </div>
+
+        <nav className="">
+          <Link href="#features" className={`font-medium uppercase ${scrolled ? 'text-black' : 'text-black'}`}>
+            Loora for business
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google';
 import "./globals.css";
+import './custom.css';
+import 'animate.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
